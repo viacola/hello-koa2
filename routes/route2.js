afe023c968da76842737eecc0ec628708e06e0b6
陈兵 <macos@white-MacBook.local>
2017-02-22 17:52:55 +0800

    删除无用的代码

--- routes/route2.js
+++ routes/route2.js
@@ -1,7 +1,5 @@
 const Router = require('koa-router');
 const router = new Router();
-//const productController = require('../controllers/productController');
-//router.get('/products', productController.findProducts);
 router.get('/route2/abc', async(ctx, next) => {
     ctx.body = 'route2/abc';
 });
